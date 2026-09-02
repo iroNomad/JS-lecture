c523ae2fedb98cad8565719c6eb17d8b35dc2e3b
iroNomad <89756508+iroNomad@users.noreply.github.com>
2026-09-02 14:46:27 +0900

    add modal functionality to display Pokémon details on click in pokemon1.js

--- pokemon/pokemon1.js
+++ pokemon/pokemon1.js
@@ -2,6 +2,9 @@
 const pokeContainer = document.querySelector('.pokemon-container');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
+const modal = document.querySelector('#modal');
+const overlay = document.querySelector('#overlay');
+const closeModalBtn = document.querySelector('#closeModal');
 
 // 전역 변수 선언
 const url = 'https://pokeapi.co/api/v2/pokemon';
@@ -23,8 +26,9 @@ const renderPokemonList = async (pokemonList) => {
 
         const newDiv = document.createElement('div');
         newDiv.classList.add('pokemon');
+        newDiv.dataset.name = pokemon.name;
         newDiv.innerHTML = `
-            <img src=${imgSrc} alt="pokemon image">
+            <img src=${imgSrc} alt=${pokemon.name} />
             <h3>${pokemon.name}</h3>
         `;
 
@@ -62,6 +66,35 @@ nextBtn.addEventListener('click', () => {
     pokeContainer.innerHTML = '';
     getPokemon();
 });
+
+//모달 열기 함수
+const openModal = async (pokemonName) => {
+    modal.style.display = 'flex';
+    overlay.style.display = 'flex';
+
+    console.log(`${url}/${pokemonName}`);
+    const response = await fetch(`${url}/${pokemonName}`);
+    const pokemonData = await response.json()
+    console.log(pokemonData.name);
+}
+//모달 닫기 함수
+const closeModal = () => {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+//모달 닫기 버튼 클릭 이벤트
+closeModalBtn.addEventListener('click', closeModal);
+
+//포콧몬을 클릭하면 실행할 이벤트
+pokeContainer.addEventListener('click', (event) => {
+    // 1. Find the nearest ancestor with the class 'pokemon'
+    const pokemonCard = event.target.closest('.pokemon');
+    // 2. Guard clause: If the click wasn't inside a .pokemon card, exit early
+    if (!pokemonCard) return;
+
+    const pokemonName = pokemonCard.dataset.name;
+    openModal(pokemonName);
+})
 
 // 초기 실행 코드
 getPokemon();
